daeda9d5d12ad267a54099dde0593dc649903056
FiLiP798 <vacu.filip@gmail.com>
2023-10-18 10:29:53 +0200

    18.10.

--- src/filip/miny/Main.java
+++ src/filip/miny/Main.java
@@ -2,6 +2,8 @@ package filip.miny;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("");
+        Policko prve = new Policko(true,7);
+        Policko druhe = new Policko(false,3);
+
     }
 }
