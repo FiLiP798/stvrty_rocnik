3cd6c12f3085de5bd58e78bd187ceb0e0243b5ef
FiLiP798 <vacu.filip@gmail.com>
2023-11-08 10:53:33 +0100

    8.11.

--- src/filip/miny/HraMiny.java
+++ src/filip/miny/HraMiny.java
@@ -11,20 +11,67 @@ public class HraMiny {
     private Policko[][] policka;
     private int pocet;
 
-
     public HraMiny(int pocetRiadkov, int pocetStlpcov, ArrayList<Pozicia> miny) {
         this.policka = new Policko[pocetRiadkov][pocetStlpcov];
         for (int riadok = 0; riadok < pocetRiadkov; riadok++) {
             for (int stlpec = 0; stlpec < pocetStlpcov; stlpec++) {
                 boolean jeTamMina = this.obsahujeMinu(riadok,stlpec,miny);
-
-                this.policka[riadok][stlpec] = new Policko(jeTamMina,-1);
+                int pocetMinVOkoli = this.spocitajMinyVOkoli(riadok, stlpec, miny);
+                this.policka[riadok][stlpec] = new Policko(jeTamMina,pocetMinVOkoli);
             }
         }
-
     }
 
-    public boolean obsahujeMinu(int riadok, int stlpec, ArrayList<Pozicia> miny) {
+    public int getPocetRiadkov() {
+        return this.policka.length;
+    }
+
+    public int getPocetStlpcov() {
+        return this.policka[0].length;
+    }
+
+    public void debugPrint(){
+        for (int riadok = 0; riadok < this.getPocetRiadkov(); riadok++) {
+            for (int stlpec = 0; stlpec < this.getPocetStlpcov(); stlpec++) {
+                if (this.policka[riadok][stlpec].maMinu()) {
+                    System.out.print("*" + " ");
+                } else {
+                    System.out.print(this.policka[riadok][stlpec].getPocet_min_okolo() + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    private int spocitajMinyVOkoli(int riadok, int stlpec, ArrayList<Pozicia> miny) {
+        int pocet = 0;
+        for (int dr = -1; dr <= 1; dr++) {
+            for (int ds = -1; ds <= 1; ds++) {
+                if (dr == 0 && ds == 0) {
+                    continue;
+                }
+                int susednyRiadok = riadok + dr;
+                int susednyStlpec = stlpec + ds;
+
+                if (!this.jeVHracomPoli(susednyRiadok,susednyStlpec)) {
+                    continue;
+                }
+                if (this.obsahujeMinu(susednyRiadok, susednyStlpec,miny)) {
+                    pocet++;
+                }
+            }
+        }
+        return pocet;
+    }
+
+    private boolean jeVHracomPoli(int riadok, int stlpec) {
+        return riadok >= 0 &&
+                stlpec >= 0 &&
+                riadok < this.getPocetRiadkov() &&
+                stlpec < this.getPocetStlpcov();
+    }
+
+    private boolean obsahujeMinu(int riadok, int stlpec, ArrayList<Pozicia> miny) {
         for (Pozicia pozicia : miny) {
             if (pozicia.jeRovnaka(riadok, stlpec)) {
                 return true;
--- src/filip/miny/Main.java
+++ src/filip/miny/Main.java
@@ -5,14 +5,13 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         ArrayList<Pozicia> miny = new ArrayList<Pozicia>();
-        miny.add(new Pozicia(3,5));
-        miny.add(new Pozicia(0,0));
         miny.add(new Pozicia(1,2));
+        miny.add(new Pozicia(2,0));
+        miny.add(new Pozicia(3,3));
 
 
-        HraMiny hra = new HraMiny(4,8, miny);
-
-        System.out.println("ppp");
+        HraMiny hra = new HraMiny(4,4, miny);
+        hra.debugPrint();
 
 
     }
